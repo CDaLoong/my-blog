ee524faebd83ded23b304cf83f3c7c13331ab13a
iDaLoong <iDaLoong@outlook.com>
2021-03-16 23:14:25 +0800

    feat: HomePage

--- src/views/NavList/HomePage.vue
+++ src/views/NavList/HomePage.vue
@@ -1,18 +1,39 @@
 <template>
-    <div class="main">
-
+  <div class="main">
+    <div class="carousel">
+      <el-carousel :interval="4000" height="calc(100vh - 240px)">
+        <el-carousel-item v-for="(item, index) in imageUrl" :key="index">
+          <img :src="item.url" :alt="item.title" style="height: calc(100vh - 240px);">
+        </el-carousel-item>
+      </el-carousel>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'HomePage',
+  data () {
+    return {
+      imageUrl: []
+    }
+  },
+  mounted () {
+    for (let i = 1; i <= 6; i++) {
+      this.imageUrl.push({ url: require(`../../assets/public/images/${i}.png`), title: `${i}.png` })
+    }
+    console.log(this.imageUrl)
+  }
 }
 </script>
 
 <style scoped lang="scss">
-    .main{
-        width: 100%;
-        height: 500px;
-    }
+.main {
+  padding-top: 36px;
+  text-align: center;
+  .carousel {
+    display: inline-block;
+    width: 66%;
+  }
+}
 </style>
--- src/views/NavList/WishingWall.vue
+++ src/views/NavList/WishingWall.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-  name: 'VowWall'
+  name: 'WishingWall'
 }
 </script>
 
